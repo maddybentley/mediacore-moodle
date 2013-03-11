261ffcbeec63154ebb7d8805aaeb5ee34fcb98b3
Jonas Pfenniger <jonas@pfenniger.name>
2013-03-11 18:31:53 +0000

    New version compatible with 2.4 and chooser-enabled

---
+++ mediacoreinsert/tinymce/editor_plugin.js
@@ -0,0 +1,59 @@
+/**
+ * @author MediaCore <info@mediacore.com>
+ */
+
+(function() {
+    // Load chooser
+    // FIXME: use some site config for the url
+    var script = document.createElement('script');
+    script.src = "http://localhost:8080/api/chooser.js";
+    (document.body || document.head || document.documentElement).appendChild(script);
+
+    tinymce.PluginManager.requireLangPack('mediacoreinsert');
+
+    tinymce.create('tinymce.plugins.MediaCoreInsertPlugin', {
+        init : function(ed, pluginUrl) {
+            var t = this;
+            t.editor = ed;
+            t.url = pluginUrl;
+            t.chooser = null;
+
+            // Register commands.
+            ed.addCommand('mceMediaCoreInsert', function() {
+                if (!t.chooser) {
+                    t.chooser = mediacore.chooser.init();
+                    t.chooser.on('media', function(media) {
+                        console.log("Got media", media);
+                        var el = t.editor.dom.createHTML('a', {href : media.url}, media.title);
+                        var out = t.editor.execCommand('mceInsertContent', false, el);
+                        console.log("out", el, out);
+                    });
+                    t.chooser.on('error', function(err) {
+                        throw err;
+                    });
+                }
+                t.chooser.open();
+            });
+
+            // Register buttons.
+            ed.addButton('mediacoreinsert', {
+                    title : 'mediacoreinsert.desc',
+                    image : t.url + '/img/icon.png',
+                    cmd : 'mceMediaCoreInsert'});
+
+        },
+
+        getInfo : function() {
+            return {
+                longname : 'MediaCore media',
+                author : 'MediaCore <info@mediacore.com>',
+                authorurl: 'http://mediacore.com',
+                version : "1.0"
+            };
+        }
+
+    });
+
+    // Register plugin.
+    tinymce.PluginManager.add('mediacoreinsert', tinymce.plugins.MediaCoreInsertPlugin);
+})();
